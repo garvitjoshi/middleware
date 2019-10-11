839db6a8c7f217ce296181c8a191d0b9bfaff5b0
Garvit Joshi <gajoshi@adobe.com>
2019-10-11 14:41:15 +0530

    check client_id in header

--- src/helpers/verifyToken.ts
+++ src/helpers/verifyToken.ts
@@ -1,9 +1,16 @@
 import * as jwt from 'jwt-then';
 
+import ClientService from "../apiV1/clients/client.service";
 import config from '../config/config';
+
+const clientService = new ClientService();
 
 const verifyToken = async (req, res, next): Promise<any> => {
   // check header or url parameters or post parameters for token
+
+  if ((typeof req.headers.client_id !== 'string') && (!req.headers.client_id)) {
+    return res.status(400).send({ auth: false, message: 'No client_id provided.' });
+  }
 
   if (typeof req.headers.authorization !== 'string') {
     return res.status(400).send({ auth: false, message: 'No authorization provided.' });
@@ -16,11 +23,23 @@ const verifyToken = async (req, res, next): Promise<any> => {
   }
 
   try {
-    // verifies secret and checks exp
+    // Check Client is exist or not.
+    const client_id = req.headers.client_id;
+    const client = await clientService.findOneByClientId(req, res, client_id);
+    if (!client.length) {
+      return res.status(403).send({ auth: false, message: 'client_id not found in database.' });
+    }
+
+    // verifies secret and checks token expiry
     const decoded = await jwt.verify(token, config.JWT_ENCRYPTION);
-    res.locals.client_id = decoded.client_id;
-    console.log(res.locals.client_id);
-    next();
+
+    if(decoded.client_id == client_id){
+      res.locals.client_id = decoded.client_id;
+      console.log(res.locals.client_id);
+      next();
+    }else{
+      return res.status(403).send({ auth: false, message: 'Token is not match with the client_id' });
+    }
   } catch (err) {
     res.status(500).send({ auth: false, message: err });
   }
